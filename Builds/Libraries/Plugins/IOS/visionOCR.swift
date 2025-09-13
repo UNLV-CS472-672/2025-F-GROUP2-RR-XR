0f2482f1f33b7f264bd268dab706875273969a98
qyamas45 <qyamas@hotmail.com>
2025-09-13 01:08:59 -0700

    Succesfully created work environment and enabled the app to run on an IOS device.

---
+++ Builds/Libraries/Plugins/IOS/visionOCR.swift
@@ -0,0 +1,56 @@
+import Foundation
+import Vision
+import UIKit
+
+@objc public class VisionOCRPlugin: NSObject {
+
+    @objc public static let shared = VisionOCRPlugin()
+    private var latestText: String = ""
+
+    @objc public func getLatestText() -> String {
+        return latestText
+    }
+
+    @objc(recognizeText:roiX:roiY:roiW:roiH:)
+    public func recognizeText(from image: CGImage,
+                              roiX: CGFloat,
+                              roiY: CGFloat,
+                              roiW: CGFloat,
+                              roiH: CGFloat) {
+        // Create the Vision request
+        let request = VNRecognizeTextRequest { [weak self] request, error in
+            guard let self = self,
+                  let observations = request.results as? [VNRecognizedTextObservation] else { return }
+
+            self.latestText = observations
+                .compactMap { $0.topCandidates(1).first?.string }
+                .joined(separator: " ")
+
+            //print("OCR Result: \(self.latestText)")
+        }
+        request.recognitionLevel = .accurate
+        request.usesLanguageCorrection = false
+
+        // Prepare cropped image if ROI is valid
+        var targetImage: CGImage? = image
+        let roi = CGRect(x: roiX, y: roiY, width: roiW, height: roiH)
+        if roi.width > 0, roi.height > 0, let cropped = image.cropping(to: roi) {
+            targetImage = cropped
+        }
+
+        // Run Vision on background queue
+ 
+        DispatchQueue.global(qos: .userInitiated).async {
+
+            if let cgImage = targetImage {
+                let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+                
+                do {
+                    try handler.perform([request])
+                } catch {
+                    print("OCR failed")
+                }
+            }
+        }
+    }
+}
