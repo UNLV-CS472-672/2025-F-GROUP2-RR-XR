f0e97d37265bf6170d262bda58dcf1bd60cc0a55
qyamas45 <qyamas@hotmail.com>
2025-10-14 14:26:27 -0700

    Changed the landscape of the environment, included some walls/occlusions

---
+++ Assets/Scripts/debugCamera.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+ 
+using Unity.Collections;
+public class debugCamera : MonoBehaviour
+
+{
+    [SerializeField] private ARCameraManager arCamManager;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
